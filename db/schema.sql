804219f5dce391741ac53ac4d3937aedf56c1ea3
William Blake <willblakebooking@gmail.com>
2018-02-20 12:55:15 -0600

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-  id int NOT NULL AUTO_INCREMENT,
-  burger_name VARCHAR(255),
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR (255),
   devoured BOOLEAN,
   PRIMARY KEY (id)
-)
+);
