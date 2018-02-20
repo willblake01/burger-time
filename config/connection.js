7d3fa16c573e73f1971a47617714365d6169394a
William Blake <willblakebooking@gmail.com>
2018-02-20 12:27:21 -0600

    setting up JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  port: 8889,
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'burgers_db'
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 8889,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function (err) {
   if (err) {
