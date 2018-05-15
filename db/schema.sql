03b2935b6274ae7b7b8985f0103d39291b04adb7
William Blake <willblakebooking@gmail.com>
2018-05-15 18:48:20 -0500

    resolved directory href error

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,13 @@
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
+CREATE DATABASE IF NOT EXISTS burgers_db;
 USE burgers_db;
 
+-- If the table already exists, remove it before trying to create the table again
+DROP TABLE IF EXISTS burgers;
+
+-- Create the burgers table
 CREATE TABLE burgers (
-  id INT AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR (255),
-  devoured BOOLEAN,
-  PRIMARY KEY (id)
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOL DEFAULT false,
+    PRIMARY KEY (id)
 );
