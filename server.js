324aa2ee63323bfd86e83f62e7e003eb873ffdff
William Blake <gwilliamblake@gmail.com>
2019-07-05 14:43:50 -0500

    dependencies update

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-var express = require('express');
-var methodOverride = require('method-override');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-var app = express();
+const app = express();
 
 // Serve static content for the app from the 'public' directory in the application directory.
 app.use(express.static('public'));
@@ -16,16 +15,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Set Handlebars
-var exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them
-var routes = require('./controllers/burgers-controller.js');
+const routes = require('./controllers/burgers-controller.js');
 
 app.use(routes);
 
 app.listen(PORT, function () {
-  console.log('App now listening at localhost: ' + PORT);
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
