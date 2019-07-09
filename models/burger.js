b61dcede9ccbe8d40e875b881757dfe8b3bc2fee
William Blake <gwilliamblake@gmail.com>
2019-07-09 14:15:22 -0500

    fix data model

--- models/burger.js
+++ models/burger.js
@@ -2,23 +2,23 @@
 var orm = require('../config/orm.js');
 
 var burger = {
-  // The variables cols and vals are arrays.
   all: function (cb) {
-    orm.all('burgers', function (res) {
+    orm.all("burgers", function (res) {
       cb(res);
     });
   },
-
-  create: function (cols, vals, cb) {
-    orm.create('burgers', cols, vals, function (res) {
-      cb(res);
-    });
+  create: function (name, cb) {
+    orm.create("burgers", [
+      "burger_name", "devoured"
+    ], [
+      name, false
+    ], cb);
   },
-
-  update: function (objColVals, condition, cb) {
-    orm.update('burgers', objColVals, condition, function (res) {
-      cb(res);
-    });
+  update: function (id, cb) {
+    var condition = "id=" + id;
+    orm.update("burgers", {
+      devoured: true
+    }, condition, cb);
   }
 };
 
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them
-const routes = require('./controllers/burgers-controller.js');
+const routes = require('./controllers/burgersController.js');
 
 app.use(routes);
 
